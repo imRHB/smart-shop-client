4bc3fffb29699d8b546d3c56e9b87d36272b1374
Rakib Hasan Babu <rhbabu3@gmail.com>
2022-02-08 17:11:37 +0600

    firebase initialized and deployed

---
+++ src/Firebase/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDhhkJUV0IRHRes7y0-Zt4FkYnQ0yogeBU",
+    authDomain: "smart-shop-pos.firebaseapp.com",
+    projectId: "smart-shop-pos",
+    storageBucket: "smart-shop-pos.appspot.com",
+    messagingSenderId: "985453297716",
+    appId: "1:985453297716:web:efc7b72848301437fd4a61"
+};
+
+export default firebaseConfig;
