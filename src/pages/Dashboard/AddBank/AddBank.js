eb02c1ae383724c5f3e71a9a458132973e389e67
Rakib Hasan Babu <rhbabu3@gmail.com>
2022-02-08 14:13:05 +0600

    added back

---
+++ src/pages/Dashboard/AddBank/AddBank.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import styles from './AddBank.module.css';
+
+const AddBank = () => {
+    return (
+        <div>
+            <h3>this is back</h3>
+            <h4>more bank</h4>
+        </div>
+    );
+};
+
+export default AddBank;
