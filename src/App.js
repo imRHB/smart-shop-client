2f0214d9dcbe3dc7cd09270a1f668b8433fd08fe
afsumeem <afsumeem@gmail.com>
2022-02-08 20:31:17 +0600

    update form

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import AddSupplier from './pages/Dashboard/AddSupplier/AddSupplier';
 
 function App() {
   return (
@@ -10,7 +11,7 @@ function App() {
       <p>Don't forget to remove your component before git push</p>
 
       {/* TEST YOUR COMPONENT HERE */}
-
+      <AddSupplier />
     </div>
   );
 }
