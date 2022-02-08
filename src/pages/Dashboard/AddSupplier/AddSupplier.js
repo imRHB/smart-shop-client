71c5f12200ed7e98e088381b95346fe8a19e9056
afsumeem <afsumeem@gmail.com>
2022-02-08 20:13:28 +0600

    create login page and add supplier page

---
+++ src/pages/Dashboard/AddSupplier/AddSupplier.js
@@ -0,0 +1,63 @@
+import React from 'react';
+import { useForm } from 'react-hook-form';
+import styles from "./AddSupplier.module.css";
+import TextField from '@mui/material/TextField';
+import { Container } from '@mui/material';
+
+const AddSupplier = () => {
+    const { register, handleSubmit, reset, formState: { errors } } = useForm();
+    return (
+
+        <Container className={`${styles.addSupplier}`}>
+            <form >
+                <div className={`${''}`}>
+                    <p>Supplier Name</p>
+                    <TextField id="outlined-basic" sx={{ width: '50%' }} label="Supplier Name" variant="outlined" {...register("name", { required: true })} />
+                </div>
+
+                <div className={`${''}`}>
+                    <p>Supplier Contact No.</p>
+                    <TextField id="outlined-basic" sx={{ width: '50%' }} label="Supplier Contact No." variant="outlined" {...register("contact", { required: true })} />
+                </div>
+
+                <div className={`${''}`}>
+                    <p>Supplier Address</p>
+                    <TextField
+                        id="outlined-textarea"
+                        label="Supplier Address"
+                        sx={{ width: '50%' }}
+                        multiline
+                        {...register("address", { required: true })}
+                    />
+                </div>
+
+                <div className={`${''}`}>
+                    <p>Supplier Details</p>
+                    <TextField
+                        id="outlined-textarea"
+                        label="Supplier Details"
+                        multiline
+                        sx={{ width: '50%' }}
+                        {...register("details", { required: true })}
+                    />
+                </div>
+
+                <div className={`${''}`}>
+                    <p>Previous Balance</p>
+                    <TextField
+                        id="outlined-textarea"
+                        label="Balance"
+                        multiline
+                        sx={{ width: '50%' }}
+                        {...register("balance", { required: true })}
+                    />
+                </div>
+
+            </form>
+
+        </Container >
+
+    );
+};
+
+export default AddSupplier;
