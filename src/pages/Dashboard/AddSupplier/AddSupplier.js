4f7be4bc39d556fa50a05bc9162e4ceef3e28dbe
afsumeem <afsumeem@gmail.com>
2022-02-09 13:23:56 +0600

    login page and add supplier page final

--- src/pages/Dashboard/AddSupplier/AddSupplier.js
+++ src/pages/Dashboard/AddSupplier/AddSupplier.js
@@ -2,26 +2,31 @@ import React from 'react';
 import { useForm } from 'react-hook-form';
 import styles from "./AddSupplier.module.css";
 import TextField from '@mui/material/TextField';
-import { Box, Container } from '@mui/material';
+import { Box, Button, Container } from '@mui/material';
+import Icon from '@mui/material/Icon';
 
 const AddSupplier = () => {
     const { register, handleSubmit, reset, formState: { errors } } = useForm();
     return (
 
         <div className={`${styles.addSupplier}`}>
+            {/* add supplier form */}
 
             <form >
-                <div className={`${styles.addSupplierField}`}>
+                {/* form title */}
+                <h4 >ADD SUPPLIER</h4>
+                <hr />
+                <div className={`${styles.addSupplierField} ${'pb-4 pt-4'}`}>
                     <p>Supplier Name <span>*</span></p>
                     <TextField id="outlined-basic" sx={{ width: '400px' }} label="Supplier Name" variant="outlined" {...register("name", { required: true })} />
                 </div>
 
-                <div className={`${styles.addSupplierField}`}>
+                <div className={`${styles.addSupplierField} ${'pb-4'}`}>
                     <p>Supplier Contact No.<span>*</span></p>
                     <TextField id="outlined-basic" sx={{ width: '400px' }} label="Supplier Contact No." variant="outlined" {...register("contact", { required: true })} />
                 </div>
 
-                <div className={`${styles.addSupplierField}`}>
+                <div className={`${styles.addSupplierField} ${'pb-4'}`}>
                     <p>Supplier Address <span>*</span></p>
                     <TextField
                         id="outlined-textarea"
@@ -32,7 +37,7 @@ const AddSupplier = () => {
                     />
                 </div>
 
-                <div className={`${styles.addSupplierField}`}>
+                <div className={`${styles.addSupplierField} ${'pb-4'}`}>
                     <p>Supplier Details <span>*</span></p>
                     <TextField
                         id="outlined-textarea"
@@ -43,7 +48,7 @@ const AddSupplier = () => {
                     />
                 </div>
 
-                <div className={`${styles.addSupplierField}`}>
+                <div className={`${styles.addSupplierField} ${'pb-4'}`}>
                     <p>Previous Balance <span>*</span></p>
                     <TextField
                         id="outlined-textarea"
@@ -53,11 +58,15 @@ const AddSupplier = () => {
                         {...register("balance", { required: true })}
                     />
                 </div>
+                <div className='d-flex justify-content-between'>
+                    <Button type='submit' className={`${styles.addSupplierButton}`}>Save Supplier</Button>
+                    <Button type='submit' className={`${styles.addAnotherButton}`}>Save and Add Another</Button>
+                </div>
+
+
 
             </form>
-
         </div >
-
     );
 };
 
