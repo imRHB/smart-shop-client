2e9b5a092eb3a9b9565b9a5f1ce0b0576acfa825
mdnajmul <mdnajmulovi@gmail.com>
2022-02-10 09:32:34 +0600

    Add Employee page component initially created by najmul

---
+++ src/pages/Dashboard/AddEmployee/AddEmployee.js
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "./AddEmployee.module.css";
+
+const AddEmployee = () => {
+  return (
+    <div>
+      <h2>This is Add Employee</h2>
+    </div>
+  );
+};
+
+export default AddEmployee;
