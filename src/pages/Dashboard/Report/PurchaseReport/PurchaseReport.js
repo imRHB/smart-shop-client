c517d30a12783ce9374e5b25d1435e76dacdb70a
zahidhasan2806 <jahidh714@gmail.comm>
2022-02-10 19:28:53 +0600

    filler button in two pages

--- src/pages/Dashboard/Report/PurchaseReport/PurchaseReport.js
+++ src/pages/Dashboard/Report/PurchaseReport/PurchaseReport.js
@@ -10,8 +10,8 @@ import Typography from "@mui/material/Typography";
 import Paper from "@mui/material/Paper";
 import { Button, Container } from "@mui/material";
 import AssignmentIcon from "@mui/icons-material/Assignment";
-import styles from './SalesReport.module.css'
-const SalesReport = () => {
+import styles from './PurchaseReport.module.css'
+const PurchaseReport = () => {
     return (
         <Container sx={{ width: "100%", mb: 5 }}>
             <Box className={`${styles.topContainer}`} sx={{ display: "flex", my: 3 }}>
@@ -20,13 +20,13 @@ const SalesReport = () => {
                 </Typography>
                 <Typography>
                     <span style={{ fontSize: "26px" }}>
-                        Sales Report</span> <br />{" "}
-                    <span style={{ color: "#969494" }}>Total Sales Report</span>
+                        Purchase Report</span> <br />{" "}
+                    <span style={{ color: "#969494" }}>Total Purchase Report</span>
                 </Typography>
             </Box>
             <Button className={`${styles.filterBtn}`}>Filter</Button>
             <Box className={`${styles.tableContainer}`} sx={{ mt: 2 }}>
-                <Typography sx={{ fontWeight: "bold" }}>Sales Report</Typography>
+                <Typography sx={{ fontWeight: "bold" }}>Purchase Report</Typography>
                 <hr />
                 <TableContainer
                     component={Paper}
@@ -35,12 +35,12 @@ const SalesReport = () => {
                     <Table aria-label="simple table">
                         <TableHead className={`${styles.tableHeader}`}>
                             <TableRow>
-                                <TableCell align="left" className={`${styles.tableCell}`}>Sales Date</TableCell>
+                                <TableCell align="left" className={`${styles.tableCell}`}>Purchase Date</TableCell>
                                 <TableCell align="left" className={`${styles.tableCell}`}>
                                     Sell No
                                 </TableCell>
                                 <TableCell align="left" className={`${styles.tableCell}`}>
-                                    Customer Name
+                                    Supplier Name
                                 </TableCell>
                                 <TableCell align="right" className={`${styles.tableCell}`}>
                                     Total Amount
@@ -49,7 +49,7 @@ const SalesReport = () => {
                         </TableHead>
                         <TableBody>
                             <TableRow>
-                                <TableCell colSpan={3} align="right" sx={{ borderRight: 1 }}>Total Sales:</TableCell>
+                                <TableCell colSpan={3} align="right" sx={{ borderRight: 1 }}>Total Purchase:</TableCell>
                                 <TableCell align="right">Dynamic total </TableCell>
                             </TableRow>
                         </TableBody>
@@ -57,8 +57,8 @@ const SalesReport = () => {
                 </TableContainer>
 
             </Box>
-        </Container >
+        </Container>
     );
 };
 
-export default SalesReport;
+export default PurchaseReport;
