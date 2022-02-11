4d1de8702757d542dc81110787af34018932eebc
afsumeem <afsumeem@gmail.com>
2022-02-11 14:07:49 +0600

    supplier page update

--- src/pages/Dashboard/ManageSupplier/ManageSupplier.js
+++ src/pages/Dashboard/ManageSupplier/ManageSupplier.js
@@ -1,21 +1,34 @@
 import React from 'react';
+import Box from "@mui/material/Box";
 import Table from "@mui/material/Table";
 import TableBody from "@mui/material/TableBody";
 import TableCell from "@mui/material/TableCell";
 import TableContainer from "@mui/material/TableContainer";
 import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
+import Typography from "@mui/material/Typography";
 import Paper from "@mui/material/Paper";
 import TablePagination from "@mui/material/TablePagination";
-import { Container } from "@mui/material";
+import { Button, Container } from "@mui/material";
+import EditIcon from "@mui/icons-material/Edit";
+import Delete from "@mui/icons-material/Delete";
+import AssignmentIcon from "@mui/icons-material/Assignment";
 import suppliers from "../../../assets/data/supplier.json";
+import styles from "./ManageSupplier.module.css";
+import MenuIcon from '@mui/icons-material/Menu';
+import ReceiptIcon from '@mui/icons-material/Receipt';
+
 
 function Row(props) {
     const { supplier } = props;
 
     return (
         <React.Fragment>
-            <TableRow hover sx={{ "&:last-child td, &:last-child th": { border: 0 } }}>
+            <TableRow
+                className={`${styles.tableHover}`}
+                sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+            >
+
                 <TableCell component="th" scope="row">
                     {supplier._id}
                 </TableCell>
@@ -23,11 +36,17 @@ function Row(props) {
                 <TableCell align="center">{supplier.address}</TableCell>
                 <TableCell align="center">{supplier.contact}</TableCell>
                 <TableCell align="center">BDT {supplier.balance}</TableCell>
-                <TableCell align="center">{"Edit | Delete"}</TableCell>
+                <TableCell align="center">
+                    <EditIcon className={`${styles.editIcon}`} />
+                    <Delete className={`${styles.deleteIcon}`} />
+                </TableCell>
             </TableRow>
+
         </React.Fragment>
     );
 }
+
+
 const ManageSupplier = () => {
     const [page, setPage] = React.useState(0);
     const [rowsPerPage, setRowsPerPage] = React.useState(5);
@@ -41,19 +60,50 @@ const ManageSupplier = () => {
         setPage(0);
     };
     return (
-        <Container sx={{ width: "100%" }}>
-            <Paper elevation={3}>
-                <TableContainer component={Paper}>
+        <Container sx={{ width: "100%", mb: 5 }}>
+            <Box className={`${styles.topContainer}`} sx={{ display: "flex", my: 3 }}>
+                <Typography>
+                    <AssignmentIcon className={`${styles.assignmentIcon}`} />{" "}
+                </Typography>
+                <Typography>
+                    <span style={{ fontSize: "26px" }}>Manage Supplier</span> <br />{" "}
+                    <span style={{ color: "#969494" }}>Manage your Supplier</span>
+                </Typography>
+            </Box>
+            <Box sx={{ textAlign: "right", my: 2 }}>
+                <Button className={`${styles.paymentBtn}`} startIcon={<MenuIcon />}>Add Supplier</Button>
+                <Button className={`${styles.receiptBtn}`} startIcon={<MenuIcon />}>Supplier Ledger</Button>
+                <Button className={`${styles.paymentBtn}`} startIcon={<ReceiptIcon />}>Supplier Payment</Button>
+                <Button className={`${styles.receiptBtn}`} startIcon={<ReceiptIcon />}>Supplier Sales Details</Button>
+            </Box>
+            <Box className={`${styles.tableContainer}`}>
+                <Typography sx={{ fontWeight: "bold" }}>Manage Supplier</Typography>
+                <hr />
+                <TableContainer
+                    component={Paper}
+                    sx={{ border: 1, borderColor: "grey.300" }}
+                >
                     <Table aria-label="simple table">
-                        <TableHead>
+                        <TableHead className={`${styles.tableHeader}`}>
                             <TableRow>
 
-                                <TableCell>SL.</TableCell>
-                                <TableCell align="center">Supplier Name</TableCell>
-                                <TableCell align="center">Address</TableCell>
-                                <TableCell align="center">Mobile</TableCell>
-                                <TableCell align="center">Balance</TableCell>
-                                <TableCell align="center">Action</TableCell>
+                                <TableCell className={`${styles.tableCell}`}>SL.</TableCell>
+                                <TableCell align="center" className={`${styles.tableCell}`}>
+                                    Supplier Name
+                                </TableCell>
+                                <TableCell align="center" className={`${styles.tableCell}`}>
+                                    Address
+                                </TableCell>
+                                <TableCell align="center" className={`${styles.tableCell}`}>
+                                    Mobile
+                                </TableCell>
+                                <TableCell align="center" className={`${styles.tableCell}`}>
+                                    Balance
+                                </TableCell>
+
+                                <TableCell align="center" className={`${styles.tableCell}`}>
+                                    Action
+                                </TableCell>
                             </TableRow>
                         </TableHead>
                         <TableBody>
@@ -65,8 +115,8 @@ const ManageSupplier = () => {
                         </TableBody>
                     </Table>
                 </TableContainer>
-
                 <TablePagination
+                    className="mt-3"
                     rowsPerPageOptions={[5, 10, 15]}
                     component="div"
                     count={suppliers.length}
@@ -75,8 +125,10 @@ const ManageSupplier = () => {
                     onPageChange={handleChangePage}
                     onRowsPerPageChange={handleChangeRowsPerPage}
                 />
-            </Paper>
+            </Box>
         </Container>
+
+
     );
 };
 export default ManageSupplier;
