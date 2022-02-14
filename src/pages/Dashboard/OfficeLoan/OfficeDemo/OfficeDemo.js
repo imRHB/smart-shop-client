9973fab85338486c9d863a9e182d345d57008ca5
mdnajmul <mdnajmulovi@gmail.com>
2022-02-14 11:32:40 +0600

    Sell, Office Loan, Personal Loan Folder Structure created

---
+++ src/pages/Dashboard/OfficeLoan/OfficeDemo/OfficeDemo.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const OfficeDemo = () => {
+  return (
+    <div>
+      <h2>This is office demo component under OfficeLoan folder.</h2>
+    </div>
+  );
+};
+
+export default OfficeDemo;
