6c5367f92277e04ebec623f7f95207a271f200b3
Munni2244 <munnikhatun51544@gmail.com>
2022-02-14 17:07:36 +0600

    added officPerson and OfficeLoan

---
+++ src/pages/Dashboard/OfficeLoan/AddOfficePerson/AddOfficePerson.js
@@ -0,0 +1,81 @@
+import React from 'react';
+import { useForm } from 'react-hook-form';
+import { Link } from 'react-router-dom';
+
+const AddOfficePerson = () => {
+    const { register, handleSubmit, watch, formState: { errors } } = useForm();
+
+
+
+    const onSubmit = (data) => {
+
+    }
+
+    return (
+        <div >
+           <div style={{ backgroundColor:'white' }} className="p-1">
+           <div className='d-flex justify-content-between m-3'>
+                <div className='text-start  d-flex'>
+                    <h1>
+                        <i class="fas fa-address-card text-primary"> </i>
+                    </h1>
+              <div className='ms-3'>
+              <h5 className='fw-bold'>Person</h5>
+                    <small className=''> Add Person</small>
+              </div>
+                </div>
+                <div>
+
+                    <small className='text-primary fw-bold border p-1'>Person/Add Person</small>
+                </div>
+
+            </div>
+           </div>
+            <div className='text-end m-3'>
+                {/* <Link to="/manageProducts"> */}
+                    <button style={{ backgroundColor: '#002447' }} className='  text-light btn fw-bold py-2'>Manage Person Loan</button>
+                {/* </Link> */}
+            </div>
+            <div style={{ marginTop: '60px' , backgroundColor:'white' }} className='container '>
+
+                <div className='row shadow border pb-4 mb-3'>
+                    <h5 className='text-start p-3 border'>Please Add  <span style={{ color: 'rgb(45, 55, 72)' }}> Person</span> </h5>
+
+                    <div className='col-12 col-lg-12 col-md-12 mt-5'>
+                        <div className='text-center'>
+                            <form onSubmit={handleSubmit(onSubmit)}>
+                             
+                                        <span className='fw-bold'>Expense Item *</span>
+
+                                        <input style={{ width: '60%' }} className='field mb-3 ms-2 ' {...register("name", { required: true })} placeholder='Person Name ' /> <br />
+
+                                       
+                                        <div className='  mt-2'>
+                                            <div className=''>
+                                                <span className='fw-bold'>Phone *</span>
+                                                <input style={{ width: '60%',marginLeft: '6%' }} className='field mb-3  ' type="number" {...register("phone", { required: true })} placeholder='Phone Number' /> <br />
+                                               
+                                            </div>
+                                         
+                                            <div className='mt-2'>
+                                                <span className='fw-bold'>Address *</span>
+                                                <input style={{ width: '60%', marginLeft: '4%' }} className='field mb-3   ' type="text" {...register("address", { required: true })} placeholder='Adsrss ' /> <br />
+                                    </div>
+                                 
+                                </div>
+
+                                <input style={{ backgroundColor: '#002447' }} className=' text-light btn fw-bold  px-5 py-2' type="submit" />
+
+                            </form>
+                        </div>
+
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    );
+
+};
+
+export default AddOfficePerson;
