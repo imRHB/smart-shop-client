c8f58017ff7f869c48ae852e6270d07d0ccb53a6
mdnajmul <mdnajmulovi@gmail.com>
2022-02-14 14:53:52 +0600

    Designation page component created

---
+++ src/pages/Dashboard/HRM/Designation/Designation.js
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "./Designation.module.css";
+
+const Designation = () => {
+  return (
+    <div>
+      <h2>This is Designation.</h2>
+    </div>
+  );
+};
+
+export default Designation;
