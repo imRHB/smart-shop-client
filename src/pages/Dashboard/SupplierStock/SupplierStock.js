cc96f72f7800f12f31b96bd1e82d4e7e7e6db6c9
zahidhasan2806 <jahidh714@gmail.comm>
2022-02-14 20:03:22 +0600

    filter button added and design modified

--- src/pages/Dashboard/SupplierStock/SupplierStock.js
+++ src/pages/Dashboard/SupplierStock/SupplierStock.js
@@ -6,17 +6,19 @@ import TableContainer from "@mui/material/TableContainer";
 import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
-
+import AssignmentIcon from "@mui/icons-material/Assignment";
 import TablePagination from "@mui/material/TablePagination";
-import { Container, Link } from "@mui/material";
+import { Button, Collapse, Container, FormControl, Link, MenuItem, Select, Typography } from "@mui/material";
 import products from "../../../assets/data/products.json";
+import styles from './Supplier.module.css'
+import { Box } from '@mui/system';
 
 function Row(props) {
     const { product } = props;
 
     return (
         <React.Fragment>
-            <TableRow hover sx={{ "&:last-child td, &:last-child th": { border: 0 } }}>
+            <TableRow sx={{ "&:last-child td, &:last-child th": { border: 0 } }} className={`${styles.tableHover}`}>
 
                 <TableCell component="th" scope="row">
                     21/02/2018                </TableCell>
@@ -34,10 +36,16 @@ function Row(props) {
 }
 const SupplierStock = () => {
     const [page, setPage] = React.useState(0);
+    const [open, setOpen] = React.useState(false);
     const [rowsPerPage, setRowsPerPage] = React.useState(10);
+    const [supplier, setSupplier] = React.useState('');
 
     const handleChangePage = (event, newPage) => {
         setPage(newPage);
+    };
+
+    const handleChange = (event) => {
+        setSupplier(event.target.value);
     };
 
     const handleChangeRowsPerPage = (event) => {
@@ -46,20 +54,62 @@ const SupplierStock = () => {
     };
     return (
         <Container sx={{ width: "100%" }}>
-            <Paper>
+            <Box className={`${styles.topContainer}`} sx={{ display: "flex", my: 1 }}>
+                <Typography>
+                    <AssignmentIcon className={`${styles.assignmentIcon}`} />{" "}
+                </Typography>
+                <Typography>
+                    <span style={{ fontSize: "26px" }}>
+                        Stock Report (Supplier Wise)</span> <br />{" "}
+                    <span style={{ color: "#969494" }}>Stock Report (Supplier Wise)</span>
+                </Typography>
+            </Box>
+            <Box sx={{ textAlign: "left", mb: 1 }}>
+                <Button className={`${styles.btn}`} onClick={() => setOpen(!open)}>Filter</Button>
+                <Collapse in={open} sx={{ mt: 2, mb: 2 }} timeout="auto" unmountOnExit className={`${styles.tableContainer}`}>
+                    <FormControl fullWidth>
+                        <Box sx={{ display: "flex", alignItems: "center" }}>
+                            <Typography sx={{ textAlign: "left", mr: 2 }} style={{ fontSize: "17px" }} >
+                                Select Supplier:
+                            </Typography>
+
+
+                            <Select sx={{ minWidth: 150, mr: 1 }}
+                                labelId="demo-simple-select-autowidth-label"
+                                id="demo-simple-select-autowidth"
+                                value={supplier}
+                                label="Select One"
+                                onChange={handleChange}
+                            >
+                                <MenuItem value="Afsana">Afsana</MenuItem>
+                                <MenuItem value="Zahid">Zahid</MenuItem>
+                                <MenuItem value="Rakib">Rakib</MenuItem>
+                                <MenuItem value="Najmul">Najmul</MenuItem>
+                                <MenuItem value="Mahfuz">Mahfuz</MenuItem>
+                                <MenuItem value="Maria">Maria</MenuItem>
+                            </Select>
+                            <Button type='submit' className={`${styles.btn}`}>Search</Button>
+                        </Box>
+                    </FormControl>
+                </Collapse>
+            </Box>
+
+            <Box className={`${styles.tableContainer}`}>
+                <Typography sx={{ fontWeight: "bold", textAlign: "left" }}>Stock Report (Supplier Wise)</Typography>
+                <hr />
                 <TableContainer component={Paper}>
                     <Table aria-label="simple table">
-                        <TableHead>
+                        <TableHead className={`${styles.tableHeader}`}>
                             <TableRow hover>
-                                <TableCell align="center">SL.</TableCell>
-                                <TableCell align="center">Suite</TableCell>
-                                <TableCell align="center">Supplier</TableCell>
-                                <TableCell align="center">Sale Price</TableCell>
-                                <TableCell align="center">Supplier Price</TableCell>
-                                <TableCell align="center">Quantity per carton</TableCell>
-                                <TableCell align="center">In Ctn.</TableCell>
-                                <TableCell align="center">Out Ctn.</TableCell>
-                                <TableCell align="center">Stock</TableCell>
+                                <TableCell className={`${styles.tableCell}`} align="center">SL.</TableCell>
+                                <TableCell className={`${styles.tableCell}`} align="center">Suite</TableCell>
+                                <TableCell className={`${styles.tableCell}`} align="center">Supplier</TableCell>
+                                <TableCell className={`${styles.tableCell}`} align="center">Sale Price</TableCell>
+                                <TableCell className={`${styles.tableCell}`} align="center">Supplier Price</TableCell>
+                                <TableCell className={`${styles.tableCell}`} align="center">Quantity per carton</TableCell>
+                                <TableCell className={`${styles.tableCell}`} align="center">In Ctn.</TableCell>
+                                <TableCell className={`${styles.tableCell}`} align="center">Out Ctn.</TableCell>
+                                <TableCell className={`${styles.tableCell}`} align="center">Stock</TableCell>
                             </TableRow>
                         </TableHead>
                         <TableBody>
@@ -87,7 +137,7 @@ const SupplierStock = () => {
                     />
                 </TableContainer>
 
-            </Paper>
+            </Box>
         </Container>
     );
 };
