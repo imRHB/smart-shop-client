7b6614605a7b7e5bf8872c3281111d9768f05c73
champmahfuz <mahfuzrahman517@gmail.com>
2022-02-15 11:14:56 +0600

    created add payment folder

---
+++ src/pages/Dashboard/PersonalLoan/AddPayment/AddPayment.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './AddPayment.module.css';
+
+const AddPayment = () => {
+    return (
+        <div>
+            <h2>This is addpayment</h2>
+        </div>
+    );
+};
+
+export default AddPayment;
