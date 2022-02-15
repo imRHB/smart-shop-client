5c958513425f4a8192e1cca82d87cdad60980bf8
champmahfuz <mahfuzrahman517@gmail.com>
2022-02-15 23:11:16 +0600

    added loan section

---
+++ src/pages/Dashboard/PersonalLoan/AddLoan/AddLoan.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import styles from './AddLoan.module.css';
+const AddLoan = () => {
+    return (
+        <div>
+            <h2>This is add loan</h2>
+        </div>
+    );
+};
+
+export default AddLoan;
