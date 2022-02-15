3b3051f3063044e77ad7dfefc413c0b5eca14655
Rakib Hasan Babu <rhbabu3@gmail.com>
2022-02-15 18:53:59 +0600

    added more hooks and removed dependencies

--- src/hooks/useProducts.js
+++ src/hooks/useProducts.js
@@ -7,7 +7,7 @@ const useProducts = () => {
         fetch('http://localhost:5000/products')
             .then(res => res.json())
             .then(data => setProducts(data));
-    }, [products]);
+    }, []);
 
     return products;
 };
