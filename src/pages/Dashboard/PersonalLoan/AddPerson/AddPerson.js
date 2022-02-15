e429c6a904ab2b9f3c20dcb5520c841f66d5f8fa
champmahfuz <mahfuzrahman517@gmail.com>
2022-02-15 23:06:55 +0600

    added addperson branch

---
+++ src/pages/Dashboard/PersonalLoan/AddPerson/AddPerson.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import styles from './AddPerson.module.css';
+const AddPerson = () => {
+    return (
+        <div>
+            <h2>This is Add Person</h2>
+        </div>
+    );
+};
+
+export default AddPerson;
