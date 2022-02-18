0c237fee9bfe4ff05040b18e3ea6999007872ecc
mdnajmul <mdnajmulovi@gmail.com>
2022-02-18 14:20:38 +0600

    Dashboard component initially create

---
+++ src/pages/Dashboard/Dashboard/Dashboard.js
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "./Dashboard.module.css";
+
+const Dashboard = () => {
+  return (
+    <div>
+      <h2 style={{ marginTop: "50px" }}>This is Dashboard</h2>
+    </div>
+  );
+};
+
+export default Dashboard;
