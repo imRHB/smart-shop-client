f6804dde55bbbc161850e0ce127f2b4630b2b0f3
afsumeem <afsumeem@gmail.com>
2022-02-21 21:19:18 +0600

    implement animated number

--- src/pages/Dashboard/UIHome/TotalReports/TotalReports.js
+++ src/pages/Dashboard/UIHome/TotalReports/TotalReports.js
@@ -1,12 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Box from '@mui/material/Box';
 import Paper from '@mui/material/Paper';
 import Grid from '@mui/material/Grid';
 import styles from "./TotalReports.module.css";
 import Typography from '@mui/material/Typography';
+import AnimatedNumber from "animated-number-react";
 
 
 const TotalReports = () => {
+    // num = {
+    //     value: 150,
+    // };
+    const [num, setNum] = useState('')
+
+    const handleChange = (e) => {
+        setNum(e.target.value);
+    };
+    // const formatValue = (value) => value.toFixed(2);
     return (
         <Box sx={{ flexGrow: 1 }}>
             <Grid container spacing={2} columns={12} sx={{ marginTop: 1 }}>
@@ -17,7 +27,12 @@ const TotalReports = () => {
                         </Typography>
                         <Box className={`${styles.reports}`}>
                             <Box>
-                                <h2>m</h2>
+                                <h2>{num}</h2>
+                                <AnimatedNumber
+                                    value="120"
+                                    onChange={handleChange}
+                                // formatValue={this.formatValue}
+                                />
                             </Box>
                             <Box>
                                 <h2>m</h2>
