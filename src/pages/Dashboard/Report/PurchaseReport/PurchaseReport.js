0a84431ae5027cfcac7b98b8622af82b3944d991
zahidhasan2806 <jahidh714@gmail.com>
2022-02-22 17:43:53 +0600

    purchase report filter button cutomize

--- src/pages/Dashboard/Report/PurchaseReport/PurchaseReport.js
+++ src/pages/Dashboard/Report/PurchaseReport/PurchaseReport.js
@@ -8,10 +8,11 @@ import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
 import Typography from "@mui/material/Typography";
 import Paper from "@mui/material/Paper";
-import { Button, Container } from "@mui/material";
+import { Button, Collapse, Container, TextField } from "@mui/material";
 import AssignmentIcon from "@mui/icons-material/Assignment";
 import styles from "./PurchaseReport.module.css";
 const PurchaseReport = () => {
+  const [open, setOpen] = React.useState(false);
   return (
     <Container sx={{ width: "100%", mb: 5 }}>
       <Box className={`${styles.topContainer}`} sx={{ display: "flex", my: 3 }}>
@@ -20,10 +21,39 @@ const PurchaseReport = () => {
         </Typography>
         <Typography>
           <span style={{ fontSize: "26px" }}>Purchase Report</span> <br />{" "}
-          <span style={{ color: "#969494" }}>Total Purchase Report</span>
+          <span style={{ color: "#969494", marginLeft: "-35px" }}>Total Purchase Report</span>
         </Typography>
       </Box>
-      <Button className={`${styles.filterBtn}`}>Filter</Button>
+      <Box sx={{ textAlign: "left", mb: 1 }}>
+        <Button className={`${styles.filterBtn}`} onClick={() => setOpen(!open)}>
+          Filter
+        </Button>
+        <Collapse
+          in={open}
+          sx={{ mt: 2, mb: 2 }}
+          timeout="auto"
+          unmountOnExit
+          className={`${styles.tableContainer}`}
+        >
+          <form >
+
+            <TextField id="date" label="Start Date" type="date" sx={{ mr: 2 }}
+              InputLabelProps={{
+                shrink: true,
+              }}
+              required
+            />
+            <TextField id="date" label="End Date" type="date" sx={{ mr: 2 }}
+              InputLabelProps={{
+                shrink: true,
+              }}
+              required
+            />
+            <Button className={`${styles.searchBtn}`} type='submit'>Search</Button>
+          </form>
+        </Collapse>
+      </Box>
+      {/* <Button className={`${styles.filterBtn}`}>Filter</Button> */}
       <Box className={`${styles.tableContainer}`} sx={{ mt: 2 }}>
         <Typography sx={{ fontWeight: "bold" }}>Purchase Report</Typography>
         <hr />
@@ -53,7 +83,8 @@ const PurchaseReport = () => {
                 <TableCell colSpan={3} align="right" sx={{ borderRight: 1 }}>
                   Total Purchase:
                 </TableCell>
-                <TableCell align="right">Dynamic total </TableCell>
+                <TableCell align="right">BDT 0.00
+                </TableCell>
               </TableRow>
             </TableBody>
           </Table>
