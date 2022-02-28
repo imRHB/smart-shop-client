21c00c93795258b1d228d4758672c70a44826ddb
afsumeem <afsumeem@gmail.com>
2022-03-01 05:42:13 +0600

    button added

--- src/pages/Dashboard/UIHome/SecondaryNav/SecondaryNav.js
+++ src/pages/Dashboard/UIHome/SecondaryNav/SecondaryNav.js
@@ -3,6 +3,8 @@ import styles from "./SecondaryNav.module.css";
 import Grid from '@mui/material/Grid';
 import Box from '@mui/material/Box';
 import defaultUser from "../../../../assets/images/user.png";
+import { Button } from "@mui/material";
+import PreviewIcon from '@mui/icons-material/Preview';
 
 
 const SecondaryNav = () => {
@@ -16,10 +18,10 @@ const SecondaryNav = () => {
                                 {false ? (
                                     <img
                                         style={{
-                                            width: "50px",
-                                            height: "50px",
+                                            width: "80px",
+                                            height: "80px",
                                             borderRadius: "50%",
-                                            border: "1px solid gray"
+                                            border: "1px solid rgb(65, 65, 65)"
 
                                         }}
                                         src={""}
@@ -45,15 +47,17 @@ const SecondaryNav = () => {
 
                     </Box>
                 </Grid>
-                <Grid item xs={12} sm={6} md={6}>
+                <Grid item xs={12} sm={6} md={6} sx={{ display: "flex", flexDirection: "column", justifyContent: "center" }}>
                     <Box sx={{ textAlign: "start" }}>
                         <h4>Welcome Back! Admin</h4>
                         <p>You have 4 new notifications! Check it out!</p>
                     </Box>
 
                 </Grid>
-                <Grid item xs={12} sm={3} md={4}>
-                    <h2>hello</h2>
+                <Grid item xs={12} sm={3} md={4} sx={{ display: "flex", alignItems: "center", justifyContent: "center" }}>
+                    <Button className={`${styles.viewProfile}`} startIcon={<PreviewIcon />}>View Profile</Button>
+                    <Button className={`${styles.startChat}`} startIcon={<PreviewIcon />}>Start Chat</Button>
+
                 </Grid>
             </Grid>
         </div>
