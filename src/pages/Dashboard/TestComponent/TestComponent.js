83bb804f8e218d1d46762de412e81dc9b13239aa
afsumeem <afsumeem@gmail.com>
2022-03-01 09:21:35 +0600

    header

--- src/pages/Dashboard/TestComponent/TestComponent.js
+++ src/pages/Dashboard/TestComponent/TestComponent.js
@@ -1,15 +1,13 @@
 import React from "react";
-import SecondaryNav from "../UIHome/SecondaryNav/SecondaryNav";
 
 const TestComponent = () => {
   return (
     <div className="text-center" style={{ marginTop: "50px" }}>
-      {/* <h1>This is Test Component</h1>
+      <h1>This is Test Component</h1>
       <p>Here, You Check Component Design</p>
       <p className="text-danger fw-bold">
         Don't forget remove your component from test route
-      </p> */}
-      <SecondaryNav />
+      </p>
     </div>
   );
 };
