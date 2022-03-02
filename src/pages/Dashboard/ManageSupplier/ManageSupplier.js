945a30aac9e477cd86c336163f63219efbae4598
afsumeem <afsumeem@gmail.com>
2022-03-02 12:22:02 +0600

    modal added

--- src/pages/Dashboard/ManageSupplier/ManageSupplier.js
+++ src/pages/Dashboard/ManageSupplier/ManageSupplier.js
@@ -19,10 +19,15 @@ import MenuIcon from '@mui/icons-material/Menu';
 import ReceiptIcon from '@mui/icons-material/Receipt';
 import DeleteIcon from '@mui/icons-material/Delete';
 import CloseIcon from '@mui/icons-material/Close';
+import { useForm } from 'react-hook-form';
+import axios from 'axios';
+import TextField from "@mui/material/TextField";
+import UpgradeIcon from '@mui/icons-material/Upgrade';
 
 
 function Row(props) {
     const { supplier, setSuppliers, suppliers } = props;
+    const { register, handleSubmit, reset, formState: { errors } } = useForm();
 
     //delete supplier
     const handleDeleteSupplier = id => {
@@ -44,13 +49,24 @@ function Row(props) {
     const handleClose = () => setShow(false);
     const handleNoBtn = () => setShow(false);
 
+    const onSubmit = data => {
+
+        axios.post('https://stormy-woodland-90777.herokuapp.com/colorCastleOrders', data)
+            .then(res => {
+                if (res.data.insertedId) {
+                    alert("Service Booked Successfully!!");
+                    reset();
+                    setShow(false)
+                }
+            })
+    };
+
     return (
         <React.Fragment>
             <TableRow
                 className={`${styles.tableHover}`}
                 sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
             >
-
                 {/* <TableCell component="th" scope="row">
                     {supplier._id}
                 </TableCell> */}
@@ -59,10 +75,11 @@ function Row(props) {
                 <TableCell align="center">{supplier.contact}</TableCell>
                 <TableCell align="center">BDT {supplier.balance}</TableCell>
                 <TableCell align="center">
-                    <EditIcon className={`${styles.editIcon}`} />
+                    <EditIcon onClick={() => { setShow(true); }} className={`${styles.editIcon}`} />
                     <Delete onClick={() => { setShow(true); }} className={`${styles.deleteIcon}`} />
                 </TableCell>
             </TableRow>
+
             {/* Confirmation alert */}
             <Modal show={show} onHide={handleClose}
                 size="md"
@@ -71,19 +88,88 @@ function Row(props) {
             >
 
                 <div className={`${"modal-header"}`}>
-                    <h5 className={`${"modal-title text-danger fw-bold"}`} id="exampleModalLabel">Edit Supplier</h5>
+                    <h5 className={`${"modal-title text-danger fw-bold"}`} id="exampleModalLabel">Delete Supplier</h5>
                 </div>
 
                 <Modal.Body>Are you sure you want to delete supplier?</Modal.Body>
                 <Modal.Footer>
                     {/* confirmation button */}
                     <Button variant="outlined" className={`${styles.paymentBtn}`} onClick={() => { handleDeleteSupplier(supplier._id) }} startIcon={<DeleteIcon />}>
-                        Update
+                        Delete
                     </Button>
                     <Button className={`${styles.receiptBtn}`} startIcon={<CloseIcon />} onClick={handleNoBtn}>
                         Cancel
                     </Button>
                 </Modal.Footer>
+            </Modal>
+
+            {/* manage supplier */}
+            {/* order now form */}
+            <Modal show={show} centered onHide={handleClose}>
+                <div className='shadow rounded' style={{ 'background': 'linear-gradient(to right, #1e3c72, #2a5298)' }}>
+                    <Modal.Header closeButton>
+                        <Modal.Title style={{ color: "white" }}>Update Supplier</Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>
+                        {/* form */}
+                        <form className="pt-3 pb-5" onSubmit={handleSubmit(onSubmit)}>
+                            <Box>
+                                <TextField
+                                    id="outlined-basic"
+                                    size="small"
+                                    className={`${styles.supplierTextField}`}
+                                    label="Supplier Name"
+                                    variant="outlined"
+                                    {...register("name", { required: true })}
+                                />
+                                <TextField
+                                    id="outlined-textarea"
+                                    size="small"
+                                    label="Supplier Address"
+                                    className={`${styles.supplierTextField}`}
+                                    multiline
+                                    {...register("address", { required: true })}
+                                />
+                                <TextField
+                                    id="outlined-basic"
+                                    size="small"
+                                    className={`${styles.supplierTextField}`}
+                                    label="Supplier Contact No."
+                                    variant="outlined"
+                                    {...register("contact", { required: true })}
+                                />
+                                <TextField
+                                    id="outlined-textarea"
+                                    label="Balance"
+                                    size="small"
+                                    multiline
+                                    className={`${styles.supplierTextField}`}
+                                    {...register("balance", { required: true })}
+                                />
+                            </Box>
+                            {/* <input className="my-2 w-75 mx-auto d-block px-4 py-1 border-0 rounded" defaultValue={user.displayName} {...register("name")} />
+
+                            {details.name &&
+                                <input className="my-2 w-75 mx-auto d-block px-4 py-1 border-0 rounded" value={details.name} {...register("service", { required: true })} />
+                            }
+
+                            {details.price &&
+                                <input className="my-2 w-75 mx-auto d-block px-4 py-1 border-0 rounded" value={details.price} {...register("price", { required: true })} />
+                            } */}
+
+                            <Modal.Footer>
+                                {/* confirmation button */}
+                                <Button variant="outlined" className={`${styles.paymentBtn}`} onClick={() => { handleDeleteSupplier(supplier._id) }} endIcon={<UpgradeIcon />}>
+                                    Update
+                                </Button>
+                                <Button className={`${styles.receiptBtn}`} endIcon={<CloseIcon />} onClick={handleNoBtn}>
+                                    Cancel
+                                </Button>
+                            </Modal.Footer>
+
+                        </form>
+                    </Modal.Body>
+                </div>
             </Modal>
 
 
@@ -179,7 +265,6 @@ const ManageSupplier = () => {
                     onPageChange={handleChangePage}
                     onRowsPerPageChange={handleChangeRowsPerPage}
                 />
-
             </Box>
         </Container>
 
