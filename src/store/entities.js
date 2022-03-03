811f85c0f86a09f3a4c6aa02e66840cf7d0fca21
mdnajmul <mdnajmulovi@gmail.com>
2022-03-03 23:18:55 +0600

    supplier reducer added on entities by najmul

--- src/store/entities.js
+++ src/store/entities.js
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux";
 import employeeReducer from "./employee";
 import designationReducer from "./designation";
+import supplierReducer from "./supplier";
 
 export default combineReducers({
   employee: employeeReducer,
   designation: designationReducer,
+  supplier: supplierReducer,
 });
