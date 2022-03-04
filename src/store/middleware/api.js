0d6cd375b752d9e59dc4cb33e477448348188008
Rakib Hasan Babu <rhbabu3@gmail.com>
2022-03-04 18:31:32 +0600

    localhost replaced

--- src/store/middleware/api.js
+++ src/store/middleware/api.js
@@ -3,37 +3,37 @@ import { apiCallBegan, apiCallFailed, apiCallSucceeded } from "../api";
 
 const api =
   ({ dispatch }) =>
-  (next) =>
-  async (action) => {
-    console.log(action);
-    if (action.type !== apiCallBegan.type) return next(action);
+    (next) =>
+      async (action) => {
+        console.log(action);
+        if (action.type !== apiCallBegan.type) return next(action);
 
-    const { url, method, data, onSuccess, onError, onStart, onFailed } =
-      action.payload;
+        const { url, method, data, onSuccess, onError, onStart, onFailed } =
+          action.payload;
 
-    if (onStart) dispatch({ type: onStart });
-    next(action);
+        if (onStart) dispatch({ type: onStart });
+        next(action);
 
-    try {
-      const response = await axios.request({
-        baseURL: "https://smart-shop-pos.herokuapp.com",
-        url,
-        method,
-        data,
-      });
+        try {
+          const response = await axios.request({
+            baseURL: "https://smart-shop-pos.herokuapp.com/",
+            url,
+            method,
+            data,
+          });
 
-      // General
-      dispatch(apiCallSucceeded(response.data));
-      // Specific
-      if (onSuccess) dispatch({ type: onSuccess, payload: response.data });
-    } catch (error) {
-      if (onFailed) dispatch({ type: onFailed });
-      // General
-      dispatch(apiCallFailed(error.message));
-      // Specific
-      if (onError) dispatch({ type: onError, payload: error.message });
-    } finally {
-    }
-  };
+          // General
+          dispatch(apiCallSucceeded(response.data));
+          // Specific
+          if (onSuccess) dispatch({ type: onSuccess, payload: response.data });
+        } catch (error) {
+          if (onFailed) dispatch({ type: onFailed });
+          // General
+          dispatch(apiCallFailed(error.message));
+          // Specific
+          if (onError) dispatch({ type: onError, payload: error.message });
+        } finally {
+        }
+      };
 
 export default api;
