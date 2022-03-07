37dd7ab857d3103c9b0d4ba41f58480ee6e29523
Munni2244 <munnikhatun51544@gmail.com>
2022-03-07 12:10:21 +0600

    update addExpense

--- src/pages/Dashboard/AddExpense/AddExpense.js
+++ src/pages/Dashboard/AddExpense/AddExpense.js
@@ -1,96 +1,170 @@
-import React from 'react';
-import { useForm } from 'react-hook-form';
-import { Link } from 'react-router-dom';
-import AssignmentIcon from '@mui/icons-material/Assignment';
-import DehazeIcon from '@mui/icons-material/Dehaze';const AddExpense = () => {
-    const { register, handleSubmit, watch, formState: { errors } } = useForm();
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import { Button, Container, Grid, Input } from "@mui/material";
+import { useForm } from "react-hook-form";
+import AssignmentIcon from "@mui/icons-material/Assignment";
+import Swal from "sweetalert2";
+import styles from "./AddExpense.module.css";
+import { Link } from "react-router-dom";
 
+const AddPerson = () => {
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm();
+  const onSubmit = (data) => {
+    let { firstName, phone, address } = data;
+    const name = `${firstName}`;
 
-
-    const onSubmit = (data) => {
-
-    }
-
-    return (
-        <div>
-            <div style={{backgroundColor:'white'}} className='d-flex justify-content-between m-3'>
-                <div className=' d-flex text-start ms-2'>
-                <h1>
-                    <AssignmentIcon style={{fontSize:'55px', backgroundColor:'#002447', color:'white'}}></AssignmentIcon>
-                    </h1>
-                  <div className='ms-2'>
-                  <h5 className='fw-bold'>EXPENSE</h5>
-                    <small className=''> Add Expense</small>
-                  </div>
-                 
-                </div>
-                <div>
-
-                    <small className=' fw-bold border p-1'>Expense/Add Expense</small>
-                </div>
-
-            </div>
-            <div className='text-end m-3'>
-                <Link to="/expenseItem">
-                    <button style={{ backgroundColor: '#002447' }} className='  text-light btn fw-bold py-1'><DehazeIcon></DehazeIcon> Add Expense</button>
-                </Link>
-            </div>
-            <div style={{ marginTop: '60px' , backgroundColor:'white'}} className='container '>
-
-                <div className='row shadow border pb-4 mb-3'>
-                    <h5 className='text-start p-3 border'>Please Add  <span style={{ color: 'rgb(45, 55, 72)' }}> Expense</span> </h5>
-
-                    <div className='col-12 col-lg-12 col-md-12 mt-5'>
-                        <div className='text-center'>
-                            <form onSubmit={handleSubmit(onSubmit)}>
-                             
-                                        <span className='fw-bold'>Expense Item *</span>
-
-                                        {/* <input style={{ width: '60%' }} className='field mb-3 ms-2 ' {...register("item", { required: true })} placeholder='Item Name ' /> <br /> */}
-
-                                        <select style={{ width: '60%' }} className='field p-1 ms-2 mb-3 ' {...register("item")}>
-                                                <option value="">Select Expanse...</option>
-                                                <option value="Camping">Lunch</option>
-                                                <option value="Cities">Snacks</option>
-                                                <option value="Trips">Breakfast</option>
-                                                <option value="Vacation">Electricity</option>
-                                                <option value="Holiday">Salary</option>
-                                                <option value="Advanture">Bounus</option>
-                                                <option value="Advanture">Food</option>
-                                                <option value="Advanture">Shop</option>
-                                                <option value="Advanture">Water</option>
-                                                <option value="Advanture">Gass</option>
-                                            </select>
-                                        {/* error */}
-                                       
-                                        <div className='  mt-2'>
-                                            <div className=''>
-                                                <span className='fw-bold'>Date *</span>
-                                                <input style={{ width: '60%',marginLeft: '6%' }} className='field mb-3 ' type="date" {...register("date", { required: true })} placeholder='Date' /> <br />
-                                               
-                                            </div>
-                                         
-                                            <div className='mt-2'>
-                                                <span className='fw-bold'>Amount *</span>
-                                                <input style={{ width: '60%', marginLeft: '4%' }} className='field mb-3   ' type="number" {...register("cost", { required: true })} placeholder='Expense Amount ' /> <br />
-
-                                         
-                                    </div>
-                                 
-                                </div>
-
-                                <input style={{ backgroundColor: '#002447' }} className=' text-light btn fw-bold  px-5 py-2' type="submit" />
-
-                            </form>
-                        </div>
-
+    const formData = new FormData();
+    formData.append("name", name);
+    formData.append("phone", phone);
+    formData.append("address", address);
+  };
+  return (
+    <Container sx={{ width: "100%", mb: 5 }}>
+      <Box className={`${styles.topContainer}`} sx={{ display: "flex", my: 3 }}>
+        <Typography>
+          <AssignmentIcon className={`${styles.assignmentIcon}`} />{" "}
+        </Typography>
+        <Typography>
+          <span style={{ fontSize: "26px" }}>Expense Item</span> <br />
+          <span style={{ color: "#969494" }}>Add Expense</span>
+        </Typography>
+      </Box>
+      <Box sx={{ textAlign: "right", my: 2 }}>
+        <Link to="manageExpense">
+        <Button className={`${styles.manageEmployeeBtn}`}>Manage Expense</Button>
+        </Link>
+      </Box>
+      <Box className={`${styles.tableContainer}`}>
+        <Typography sx={{ fontWeight: "bold" , textAlign:'start', marginLeft:'6px'}}>Add Expense Item</Typography>
+        <hr />
+        <div className="mt-2">
+          <div className="form-container">
+            <div>
+              <form onSubmit={handleSubmit(onSubmit)}>
+                <div className="row gx-3 mb-3">
+                  <div className="col-lg-6 col-md-6 col-sm-12 col-12">
+                    <div className="p-3 border bg-light">
+                      <div className="mb-3">
+                        <label
+                          className="form-label"
+                          style={{ fontWeight: "bold" }}
+                        >
+                          Expense Name <sup className="text-danger fw-bold fs-6">*</sup>
+                        </label>
+                        <input
+                          type="text"
+                          className="form-control"
+                          placeholder="Name"
+                          style={{ background: "#E5E5E5" }}
+                          {...register("firstName", { required: true })}
+                        />
+                        {errors.firstName && (
+                          <span className="text-danger">
+                            Please enter Expense name.
+                          </span>
+                        )}
+                      </div>
                     </div>
-
+                    <div className="p-3 border bg-light mt-4">
+                      <div className="mb-3">
+                        <label
+                          className="form-label"
+                          style={{ fontWeight: "bold" }}
+                        >
+                          Date{" "}
+                          <sup className="text-danger fw-bold fs-6">*</sup>
+                        </label>
+                        <input
+                          type="Date"
+                          className="form-control"
+                          placeholder="Date"
+                          style={{ background: "#E5E5E5" }}
+                          {...register("date", { required: true })}
+                        />
+                        {errors.phone && (
+                          <span className="text-danger">
+                            Please enter A Date.
+                          </span>
+                        )}
+                      </div>
+                    </div>
+                    <div className="p-3 border bg-light mt-4">
+                      <div className="mb-3">
+                        <Box sx={{ textAlign: "center", my: 2 }}>
+                          <input
+                            type="reset"
+                            className={`${"btn"} ${styles.resetBtn}`}
+                            style={{ background: "#251D58", color: "#fff" }}
+                            value="Reset"
+                          />
+                          <input
+                            type="submit"
+                            className={`${"btn"} ${styles.saveBtn}`}
+                            style={{ background: "#251D58", color: "#fff" }}
+                            value="Save"
+                          />
+                        </Box>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="col-lg-6 col-md-6 col-sm-12 col-12 ">
+                    <div className="p-3 border bg-light">
+                      <div className="mb-3">
+                        <label
+                          className="form-label"
+                          style={{ fontWeight: "bold" }}
+                        >
+                          Amount{" "}
+                          <sup className="text-danger fw-bold fs-6">*</sup>
+                        </label>
+                        <input
+                          type="number"
+                          className="form-control"
+                          placeholder="Amount"
+                          style={{ background: "#E5E5E5" }}
+                          {...register("amount", { required: true })}
+                        />
+                        {errors.phone && (
+                          <span className="text-danger">
+                            Please enter A Amount.
+                          </span>
+                        )}
+                      </div>
+                    </div>
+                    <div className="p-3 border bg-light mt-4">
+                      <div className="mb-3">
+                        <label
+                          className="form-label"
+                          style={{ fontWeight: "bold" }}
+                        >
+                          Details
+                        </label>
+                        <textarea
+                          className="form-control"
+                          rows="3"
+                          placeholder="Details"
+                          style={{ background: "#E5E5E5" }}
+                          {...register("details", { required: false })}
+                        ></textarea>
+                      </div>
+                    </div>
+                   
+                  </div>
+                
                 </div>
-            </div>
-        </div>
-    );
 
+              </form>
+            </div>
+          </div>
+        </div>
+      </Box>
+    </Container>
+  );
 };
 
-export default AddExpense;
+export default AddPerson;
