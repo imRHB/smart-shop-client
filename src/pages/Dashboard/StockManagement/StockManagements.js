3308a8b0ca767166e7a331e03cb666861b1b4e4d
zahidhasan2806 <jahidh714@gmail.com>
2022-03-07 23:45:32 +0600

    fix issue

--- src/pages/Dashboard/StockManagement/StockManagements.js
+++ src/pages/Dashboard/StockManagement/StockManagements.js
@@ -50,10 +50,10 @@ function Row(props) {
                     <Collapse in={open} timeout="auto" unmountOnExit>
                         <Box sx={{ margin: 1 }}>
                             <Typography variant="h6" gutterBottom component="div">
-                                product Details
+                                Product Details
                             </Typography>
                             <Table size="small" aria-label="purchases">
-                                <TableHead>
+                                <TableHead >
                                     <TableRow>
                                         <TableCell align="center">Product Name</TableCell>
                                         <TableCell align="center">Category</TableCell>
