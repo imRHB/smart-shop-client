139ac0a260e40c0801154fd4a53aeb4c572ae13b
mdnajmul <mdnajmulovi@gmail.com>
2022-03-09 17:43:29 +0600

    create purchase product page & initially import some dependencies

---
+++ src/pages/Dashboard/Products/PurchaseProduct/PurchaseProduct.js
@@ -0,0 +1,29 @@
+import React, { useState } from "react";
+import { useForm } from "react-hook-form";
+import styles from "./PurchaseProduct.module.css";
+import TextField from "@mui/material/TextField";
+import { Box, Button, Container } from "@mui/material";
+import Typography from "@mui/material/Typography";
+import AssignmentIcon from "@mui/icons-material/Assignment";
+import MenuIcon from "@mui/icons-material/Menu";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import Paper from "@mui/material/Paper";
+import TableRow from "@mui/material/TableRow";
+import Delete from "@mui/icons-material/Delete";
+import Collapse from "@mui/material/Collapse";
+import Swal from "sweetalert2";
+import { NavLink } from "react-router-dom";
+
+const PurchaseProduct = () => {
+  return (
+    <div>
+      <h2>Purchase Product Invoice</h2>
+    </div>
+  );
+};
+
+export default PurchaseProduct;
