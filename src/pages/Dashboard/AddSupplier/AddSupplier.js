a71faddf615bb64bc4851924e2c9e20f9d3e2b8a
afsumeem <afsumeem@gmail.com>
2022-03-09 11:57:25 +0600

    add suppliers fields updated

--- src/pages/Dashboard/AddSupplier/AddSupplier.js
+++ src/pages/Dashboard/AddSupplier/AddSupplier.js
@@ -8,10 +8,13 @@ import AssignmentIcon from "@mui/icons-material/Assignment";
 import MenuIcon from "@mui/icons-material/Menu";
 import ReceiptIcon from "@mui/icons-material/Receipt";
 import Grid from "@mui/material/Grid";
-import axios from "axios";
 import Swal from "sweetalert2";
 import { useDispatch, useSelector } from "react-redux";
 import { saveSupplierToDB } from "../../../store/supplier";
+import Select from '@mui/material/Select';
+import FormControl from '@mui/material/FormControl';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from "@mui/material/MenuItem";
 
 const AddSupplier = () => {
   const dispatch = useDispatch();
@@ -33,6 +36,12 @@ const AddSupplier = () => {
       showConfirmButton: true,
     });
     reset();
+  };
+
+  const [company, setCompany] = React.useState('');
+
+  const handleCompanyChange = (event) => {
+    setCompany(event.target.value);
   };
 
   return (
@@ -78,8 +87,8 @@ const AddSupplier = () => {
             sx={{ marginTop: 2, marginBottom: 2 }}
           >
             <Grid item md={8} sm={16} sx={16}>
-              <Box className={`${styles.addSupplierField} ${"pb-4"}`}>
-                <Typography className={`${styles.addSupplierFieldLabel}`}>
+              <Box className={`${styles.addSupplierField} ${"pb-4"}`} >
+                <Typography className={`${styles.addSupplierFieldLabel} ${'mb-2'}`}>
                   Supplier Name <span>*</span>
                 </Typography>
 
@@ -93,11 +102,10 @@ const AddSupplier = () => {
                 />
               </Box>
 
-              <Box className={`${styles.addSupplierField} ${"pb-4"}`}>
+              <Box className={`${styles.addSupplierField} ${"pb-4"}`} sx={{ mt: 2 }}>
                 <Typography className={`${styles.addSupplierFieldLabel}`}>
                   Supplier Contact No. <span>*</span>
                 </Typography>
-
                 <TextField
                   id="outlined-basic"
                   size="small"
@@ -107,60 +115,57 @@ const AddSupplier = () => {
                   {...register("contact", { required: true })}
                 />
               </Box>
-
-              <Box className={`${styles.addSupplierField}`}>
-                <Typography className={`${styles.addSupplierFieldLabel}`}>
-                  Supplier Address<span>*</span>
-                </Typography>
-                <TextField
-                  id="outlined-textarea"
-                  size="small"
-                  label="Supplier Address"
-                  className={`${styles.supplierTextField}`}
-                  multiline
-                  {...register("address", { required: true })}
-                />
-              </Box>
             </Grid>
             <Grid item md={8} sm={16} sx={16}>
               <Box className={`${styles.addSupplierField} ${"pb-4"}`}>
                 <Typography className={`${styles.addSupplierFieldLabel}`}>
-                  Supplier Details<span>*</span>
+                  Company Name<span>*</span>
                 </Typography>
 
-                <TextField
-                  id="outlined-textarea"
-                  size="small"
-                  label="Supplier Details"
-                  multiline
-                  className={`${styles.supplierTextField}`}
-                  {...register("details", { required: true })}
-                />
+                <FormControl fullWidth>
+                  <InputLabel id="demo-simple-select-label">Select Company</InputLabel>
+                  <Select
+                    labelId="demo-simple-select-label"
+                    id="outlined-basic"
+                    size="small"
+                    className={`${styles.supplierTextField}`}
+                    label="Company Name"
+                    variant="outlined"
+                    {...register("company", { required: true })}
+                    value={company}
+                    sx={{ padding: "4px" }}
+                    onChange={handleCompanyChange}
+                  >
+                    <MenuItem value={1}>Cheviot International Ltd.</MenuItem>
+                    <MenuItem value={2}>Hoang Mo Thien Company Limited</MenuItem>
+                    <MenuItem value={2}>Ozege tekstil konfeksioyon sanayi ve ticaret</MenuItem>
+                    <MenuItem value={3}>Cannabis Suppliers</MenuItem>
+                    <MenuItem value={4}>Best International Ltd.</MenuItem>
+                  </Select>
+                </FormControl>
               </Box>
 
               <Box className={`${styles.addSupplierField} ${"pb-4"}`}>
                 <Typography className={`${styles.addSupplierFieldLabel}`}>
-                  Previous Balance<span>*</span>
+                  Company Address<span>*</span>
                 </Typography>
 
                 <TextField
                   id="outlined-textarea"
-                  label="Balance"
+                  label="Company Address"
                   size="small"
                   multiline
                   className={`${styles.supplierTextField}`}
-                  {...register("balance", { required: true })}
+                  {...register("companyAddress", { required: true })}
                 />
               </Box>
-              <Box className="d-flex justify-content-around mt-5">
-                <Button type="submit" className={`${styles.addSupplierButton} ${"mb-1"}`}>
-                  Save Supplier
-                </Button>
-                <Button type="submit" className={`${styles.addAnotherButton} ${"mb-1"}`}>
-                  Save and Add Another
-                </Button>
-              </Box>
             </Grid>
+            <Box className="d-block m-auto mt-2">
+              <Button type="submit" className={`${styles.addSupplierButton} ${"mb-1"}`}>
+                Save Supplier
+              </Button>
+
+            </Box>
           </Grid>
         </form>
       </Box>
