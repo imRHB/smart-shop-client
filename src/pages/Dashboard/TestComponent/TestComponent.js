02da05a6c273f43d198526d160c6b60d5f23b047
zahidhasan2806 <jahidh714@gmail.com>
2022-03-11 02:41:41 +0600

    inovice basic design

--- src/pages/Dashboard/TestComponent/TestComponent.js
+++ src/pages/Dashboard/TestComponent/TestComponent.js
@@ -1,14 +1,18 @@
 import React from "react";
+import SupplierInvoice from "../SupplierInvoice/SupplierInvoice";
 
 const TestComponent = () => {
   return (
-    <div className="text-center" style={{ marginTop: "50px" }}>
-      <h1>This is Test Component</h1>
-      <p>Here, You Check Component Design</p>
-      <p className="text-danger fw-bold">
-        Don't forget remove your component from test route
-      </p>
-    </div>
+    // <div className="text-center" style={{ marginTop: "50px" }}>
+    //   <h1>This is Test Component</h1>
+    //   <p>Here, You Check Component Design</p>
+    //   <p className="text-danger fw-bold">
+    //     Don't forget remove your component from test route
+    //   </p>
+    // </div>
+    <>
+      <SupplierInvoice />
+    </>
   );
 };
 
