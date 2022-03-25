3c293ec99cbdccca58e70a1e66bea27c4472623f
mdnajmul <mdnajmulovi@gmail.com>
2022-03-25 13:05:06 +0600

    fixed some design issue

--- src/pages/Dashboard/Customer/CustomerManagement/CustomerManagement.js
+++ src/pages/Dashboard/Customer/CustomerManagement/CustomerManagement.js
@@ -143,8 +143,8 @@ function Row(props) {
           />
         </TableCell>
       </TableRow>
-      <TableRow>
-        <TableCell style={{ paddingBottom: 0, paddingTop: 0 }} colSpan={6}>
+      <TableRow style={{ background: "#D0F2FF" }}>
+        <TableCell style={{ paddingBottom: 0, paddingTop: 0 }} colSpan={7}>
           <Collapse in={open} timeout="auto" unmountOnExit>
             <Box sx={{ margin: 1 }}>
               <Typography
